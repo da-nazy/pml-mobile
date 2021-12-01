c5d5aaa0b7b7856b8ca3c955cd80703bfbd89268
danazy <danielani660@gmail.com>
2021-12-01 17:23:27 +0100

    recent update

--- Component/Tracking/Track.js
+++ Component/Tracking/Track.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {View,Text} from 'react-native';
+import {IconComp} from '../WorkerComp/ExternalFunction';
+
 export default function Track(){
     //Should get the code from route params
     return(
         <View>
+           <View>{IconComp()}</View> 
 
         </View>
     )
