def0dd9c173133de048b785a5d23317a772cb232
da-nazy <danielani660@gmail.com>
2022-12-08 17:16:16 +0100

    delete dev

--- Component/WorkerComp/Signupbtm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { View,Text, TouchableOpacity } from 'react-native';
-
-export  const Signupbtm=({userSignUp,dispatcherSignUP})=>{
-  return(<View>
-          <TouchableOpacity onPress={(e)=>userSignUp(e)}><Text> Sign up as an Individual User</Text></TouchableOpacity>
-          <TouchableOpacity onPress={(e)=>dispatcherSignUP(e)}><Text>Sign up as Dispatcher</Text></TouchableOpacity>
-  </View>)
-}
